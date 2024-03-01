c92b944172d6422bd0ba817a1e32be6b7023162e
Kanhu Charan <kanhu2551996@gmail.com>
2024-03-01 22:49:52 +0530

    connect to firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 
 function App() {
-  const [name, setName] = useState("Krishna");
-  const [chart,setChart] = useState([{name:"User1",message:"message1"}])
+  const [name, setName] = useState("");
+  const [chat, setChat] = useState([]);
+  const [msg, setMsg] = useState("");
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
 
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, {
+      name,
+      message: msg,
+    });
+    // setChart([...chart, { name, message: msg }])
+    setMsg("");
+  };
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      console.log(data.val());
+      setChat((chat) => [...chat, data.val()]);
+    });
+  }, []);
   return (
     <div>
-      <h1>User: {name}</h1>
-      <div className="chart-container">
-      {chart.map(c=><div className={`container ${c.name ===name ?'me': ''}`}>
-       <p className='chart-box'>
-          <strong>{c.name}:</strong>
-          <span>{c.message}</span>
-        </p>
-          </div>)}
-       
+      <div>
+        {name ? null : (
+          <div>
+            <input
+              type="text"
+              placeholder="Enter Your NAme"
+              onBlur={(e) => setName(e.target.value)}
+            />
+          </div>
+        )}
       </div>
-      <div className="send">
-      <input type="text" placeholder='Enter your chart'/>
-      <button>Submit</button>
-      </div>
+      {name ? (
+        <div>
+          <h1>User: {name}</h1>
+          <div className="chart-container">
+            {chat.map((c, i) => (
+              <div
+                key={c.i}
+                className={`container ${c.name === name ? "me" : ""}`}
+              >
+                <p className="chart-box">
+                  <strong>{c.name}:</strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+          </div>
+          <div className="send">
+            <input
+              type="text"
+              placeholder="Enter your chart"
+              value={msg}
+              onInput={(e) => setMsg(e.target.value)}
+            />
+            <button onClick={(e) => sendChat()}>Submit</button>
+          </div>
+        </div>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgsQsYJdHthm1pay-MPX5KF82KkvnK-wA",
+  authDomain: "tk-survey.firebaseapp.com",
+  databaseURL: "https://tk-survey-default-rtdb.firebaseio.com",
+  projectId: "tk-survey",
+  storageBucket: "tk-survey.appspot.com",
+  messagingSenderId: "656760605822",
+  appId: "1:656760605822:web:07d3f9cbe57ed923995c2f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
